1f6c5b89a07f8e1e09546203ebff99ab9967c521
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-04-29 20:28:50 -0400

    tipo de dato null

--- leccion01/app.js
+++ leccion01/app.js
@@ -1,9 +1,10 @@
 
-var x;
+let x = null;
 
+console.log(x);
 console.log(typeof x);
 
-document.getElementById("parrafo").innerHTML = x;
+document.getElementById("parrafo").innerHTML = typeof x;
 
 
 
